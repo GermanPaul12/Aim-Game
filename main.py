24fa99443ca2b22fa2c5956bcf5dd84d88a13621
German Paul <99292019+GermanPaul12@users.noreply.github.com>
2022-09-17 14:12:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,48 +10,56 @@ root.title('Farbige Boxen')
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-root.geometry("1300x720")
+root.geometry("1400x800")
 root.configure(bg='black')
 
 color='red'
 points = 0
-start_game = False
 timer = 60
 
-random_x = np.arange(0, 1291)
-random_y = np.arange(0, 611)
+random_x = np.arange(0, 1370)
+random_y = np.arange(115, 770)
 
 
 
+
+def start_game():
+    global timer
+    root.after(1000, start_game)
+    timer -= 1
+    title_label.configure(text=f"Points= {points}                  Aim Game                    Time= {timer}")
+    if timer <= 0:
+        messagebox.showinfo("Game Over", f"Your score is {points}")
+        root.destroy()   
 
 def new_loc():
     global points
-    global timer
-    button_1.place(x=random.choice(random_x), y=random.choice(random_y), height=10, width=10)
+    button_1.place(x=random.choice(random_x), y=random.choice(random_y))
     points += 1
-    root.after(1000, timer - 1)
+    if points == 1:
+        start_game()
     title_label.configure(text=f"Points= {points}                  Aim Game                    Time= {timer}")
      
          
             
 game_frame = customtkinter.CTkFrame(master=root,
-                               width=1300,
+                               width=1400,
                                height=800,
                                corner_radius=10,
                                bg='black',
                                fg_color='black',)
 
-title_label = customtkinter.CTkLabel(root, text=f"Points= {points}                  Aim Game                    Time= 60 sec", 
-                                width=1300,
+title_label = customtkinter.CTkLabel(root, text=f"Points= {points}                  Aim Game                    Time= 60", 
+                                width=1400,
                                height=100,
                                fg_color=("white", "white"),
                                corner_radius=8, 
-                               text_font=('Times New Roman', 44),
+                               text_font=('Times New Roman', 50, 'bold'),
                                bg_color='black',
                                text_color='black')
 
-button_1 = customtkinter.CTkButton(root, command=new_loc, width=20,
-                                height=20,
+button_1 = customtkinter.CTkButton(root, command=new_loc, width=30,
+                                height=30,
                                 text="",
                                 fg_color=color,
                                 text_font=('Times New Roman', 26),
@@ -65,8 +73,8 @@ button_1 = customtkinter.CTkButton(root, command=new_loc, width=20,
 #Packing them on the screen
 
 game_frame.place(x=0, y=0)
-title_label.place(x=0, y=0, height=100, width=1300)
-button_1.place(x=100, y=100, height=20, width=20)
+title_label.place(x=0, y=0)
+button_1.place(x=100, y=100)
 
 
 
